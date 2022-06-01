8e429253356f628abd84bdb60ea243de6f08a1c6
a-sha0234 <asharma_31@hotmail.com>
2022-06-01 16:40:04 +0100

    edit app component and call other components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import AddButton from "./components/AddButton";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <AddButton />
+    </div>
+  );
 }
 
 export default App;
