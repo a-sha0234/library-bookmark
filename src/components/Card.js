412e208b2f75485add2fc0c43158587c8d96b5a5
a-sha0234 <asharma_31@hotmail.com>
2022-06-02 13:35:15 +0100

    create card component, allow users to delete cards and decide if book is finished or not

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,40 @@
-export default function Card() {}
+import { useState } from "react";
+
+export default function Card(props) {
+  const [isBookFinished, setIsBookFinished] = useState(false);
+
+  function isFinished() {
+    setIsBookFinished((prev) => {
+      return !prev;
+    });
+  }
+
+  console.log(props);
+
+  function deleteBook(e) {
+    const deleteButton = e.target.name;
+    let filteredArr = props.storeBooks.filter(
+      (item) => item.key != deleteButton
+    );
+    props.setStoreBooks(filteredArr);
+  }
+
+  return (
+    <div>
+      <section>
+        <p>{props.book.bookName}</p>
+        <p>{props.book.Author}</p>
+        <p>{props.book.numberOfPages}</p>
+      </section>
+
+      <section>
+        <button onClick={deleteBook} name={props.book.key}>
+          delete
+        </button>
+        <button onClick={isFinished}>
+          {isBookFinished === false ? "Not finished" : "Finished"}
+        </button>
+      </section>
+    </div>
+  );
+}
