dc1fda69f3779e0134da1f3ec297de2cb452eb31
a-sha0234 <asharma_31@hotmail.com>
2022-06-05 21:59:58 +0100

    add dynamic styling and classnames

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,6 +11,11 @@ export default function Card(props) {
 
   console.log(props);
 
+  const style = {
+    backgroundColor: isBookFinished === false ? "red" : "green",
+    color: "white",
+  };
+
   function deleteBook(e) {
     const deleteButton = e.target.name;
     let filteredArr = props.storeBooks.filter(
@@ -20,21 +25,31 @@ export default function Card(props) {
   }
 
   return (
-    <div>
-      <section>
-        <p>{props.book.bookName}</p>
-        <p>{props.book.Author}</p>
-        <p>{props.book.numberOfPages}</p>
-      </section>
+    <div className="bookCard">
+      <div className="bookCard__container">
+        <section className="bookCard__details">
+          <p>{props.book.bookName}</p>
+          <p>By: {props.book.Author}</p>
+          <p> pages: {props.book.numberOfPages}</p>
+        </section>
 
-      <section>
-        <button onClick={deleteBook} name={props.book.key}>
-          delete
-        </button>
-        <button onClick={isFinished}>
-          {isBookFinished === false ? "Not finished" : "Finished"}
-        </button>
-      </section>
+        <section className="bookCardButtons">
+          <button
+            onClick={deleteBook}
+            name={props.book.key}
+            className="bookCardButtons__delete"
+          >
+            delete
+          </button>
+          <button
+            onClick={isFinished}
+            style={style}
+            className="bookCardButtons__isFinished"
+          >
+            {isBookFinished === false ? "Not finished" : "Finished"}
+          </button>
+        </section>
+      </div>
     </div>
   );
 }
